3bf4a565ae689ecec525e66e0994d128b3dd759a
riapatterson <riapatterson@hotmail.co.uk>
2025-07-15 14:57:23 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const fruits = require("./routes/fruits")
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
+app.use(cors());
 app.use(express.json());
 app.use('/fruits', fruits);
 
